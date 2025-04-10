87faaccb9fbe68095adb0c8a6a2be3211578154a
Natashia Jurman <natashiajurman@gmail.com>
2025-04-09 21:17:36 -0400

    Week 14 Lecture
    Added Week 14 lecture materials, and fixed score bug.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,14 +13,25 @@ public class GameManager : MonoBehaviour
     public GameObject enemyOnePrefab;
     public GameObject cloudPrefab;
     public GameObject coinPrefab;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject powerupPrefab;
+    public GameObject audioPlayer;
+
+    public AudioClip powerupSound;
+    public AudioClip powerdownSound;
 
     public TextMeshProUGUI livesText;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI powerupText;
 
     public float horizontalScreenSize;
     public float verticalScreenSize;
 
     public int score;
+    public int cloudMove;
+
+    private bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -26,21 +39,35 @@ public class GameManager : MonoBehaviour
         horizontalScreenSize = 10f;
         verticalScreenSize = 6.5f;
         score = 0;
+        cloudMove = 1;
+        gameOver = false;
+        AddScore(0);
         Instantiate(playerPrefab, transform.position, Quaternion.identity);
         CreateSky();
         InvokeRepeating("CreateEnemy", 1, 3);
         InvokeRepeating("CreateCoin", 3, 2);
+        StartCoroutine(SpawnPowerup());
+        powerupText.text = "No powerups yet!";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     void CreateEnemy()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void CreatePowerup()
+    {
+        Instantiate(powerupPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
     }
 
     void CreateSky()
@@ -52,6 +79,50 @@ public class GameManager : MonoBehaviour
 
     }
 
+    public void ManagePowerupText(int powerupType)
+    {
+        switch (powerupType)
+        {
+            case 1:
+                powerupText.text = "Speed!";
+                break;
+            case 2:
+                powerupText.text = "Double Weapon!";
+                break;
+            case 3:
+                powerupText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerupText.text = "Shield!";
+                break;
+            default:
+                powerupText.text = "No powerups yet!";
+                break;
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        float spawnTime = Random.Range(3, 5);
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerup();
+        StartCoroutine(SpawnPowerup());
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerupSound);
+                break;
+            case 2:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerdownSound);
+                break;
+        }
+    }
+
+
     void CreateCoin()
     {
         Instantiate(coinPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.85f, Random.Range(-verticalScreenSize, verticalScreenSize) * 0.7f +1, 0), Quaternion.Euler(180, 0, 0));
@@ -60,6 +131,7 @@ public class GameManager : MonoBehaviour
     public void AddScore(int earnedScore)
     {
         score += earnedScore;
+        ChangeScoreText(score);
     }
 
     public void ChangeLivesText(int currentLives)
@@ -70,5 +142,14 @@ public class GameManager : MonoBehaviour
     public void ChangeScoreText(int currentScore)
     {
         scoreText.text = "Score: " + currentScore;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        restartText.SetActive(true);
+        gameOver = true;
+        CancelInvoke();
+        cloudMove = 0;
     }
 }
